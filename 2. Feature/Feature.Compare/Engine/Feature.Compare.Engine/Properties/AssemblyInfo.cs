252f9e22545962dba1bc32502615906b7f2be411
Rob Earlam <robert.earlam@gmail.com>
2017-11-23 17:50:21 +1100

    fixed issue with previous project rename, removed logs from git

--- 2. Feature/Feature.Compare/Engine/Feature.Compare.Engine/Properties/AssemblyInfo.cs
+++ 2. Feature/Feature.Compare/Engine/Feature.Compare.Engine/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SxaS.Compare.Engine")]
+[assembly: AssemblyProduct("Feature.Compare.Engine")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
