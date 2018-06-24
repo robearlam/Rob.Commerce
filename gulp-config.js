a23c3584ce2bb8f9c74f9c74ce174366f70856ba
Rob Earlam <robert.earlam@gmail.com>
2018-06-24 10:39:05 +1000

    Moved to source to "src" dir, updated gulp refs & tasks

--- gulp-config.js
+++ gulp-config.js
@@ -1,14 +1,14 @@
 module.exports = function () {
     var webroot = "C:\\inetpub\\wwwroot";
     var config = {
-        sitecoreRoot: webroot + "\\xp901.sc\\Website",
-        engineAuthoringRoot: webroot + "\\CommerceAuthoring_Sc9",
-        engineShopsRoot: webroot + "\\CommerceShops_Sc9",
-        engineOpsRoot: webroot + "\\CommerceOps_Sc9",
-        engineMinionsRoot: webroot + "\\CommerceMinions_Sc9",
+        sitecoreRoot: webroot + "\\xc901.sc",
+        engineAuthoringRoot: webroot + "\\xc901.CommerceAuthoring",
+        engineShopsRoot: webroot + "\\xc901.CommerceShops",
+        engineOpsRoot: webroot + "\\xc901.CommerceOps",
+        engineMinionsRoot: webroot + "\\xc901.CommerceMinions",
         solutionName: "Rob.Commerce",
-        dbSuffix: "xp901",
-        engineProjectPath: "./3. Project/Project.Commerce/Sitecore.Commerce.Engine",
+        dbSuffix: "xc901",
+        engineProjectPath: "./src/3. Project/Project.Commerce/Sitecore.Commerce.Engine",
         buildConfiguration: "Debug",
         buildToolsVersion: 15.0,
         buildMaxCpuCount: 0,
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 let { build, publish } = require('gulp-dotnet-cli');
 let gulp = require('gulp');
-let runSequence = require("run-sequence");
 let fs = require('fs');
 let del = require('del');
 let jsonModify = require('gulp-json-modify');
 let exec = require('child_process').exec;
+let msbuild = require("gulp-msbuild");
+let flatmap = require('gulp-flatmap');
+var debug = require("gulp-debug");
 
 var config;
 if (fs.existsSync('./gulp-config.js.user')) {
@@ -15,12 +17,71 @@ else {
 }
 module.exports.config = config;
 
-gulp.task("default", function (callback) {
-    return runSequence(
-        "01-Build-Commerce-Engine",
-        "02-Publish-Commerce-Engine-To-Instances",
-        callback);
+gulp.task("Publish-Foundation-Projects", function () {
+    return publishProjects("./src/1. Foundation");
 });
+
+gulp.task("Publish-Feature-Projects", function () {
+    return publishProjects("./src/2. Feature");
+});
+
+gulp.task("Publish-Project-Projects", function () {
+    return publishProjects("./src/3. Project");
+});
+
+gulp.task('Stop-Local-IIS', function (callback) {
+    exec('Powershell.exe  iisreset /stop', function (err, stdout) {
+        console.log(stdout);
+        callback(err);
+    });
+});
+
+gulp.task('Start-Local-IIS', function (callback) {
+    exec('Powershell.exe  iisreset /start', function (err, stdout) {
+        console.log(stdout);
+        callback(err);
+    });
+});
+
+gulp.task('Copy-Published-Engine-To-All-Instances', function () {
+    return gulp.src(config.engineProjectPath + "/bin/publish/**/*")
+        .pipe(gulp.dest(config.engineAuthoringRoot))
+        .pipe(gulp.dest(config.engineShopsRoot))
+        .pipe(gulp.dest(config.engineMinionsRoot))
+        .pipe(gulp.dest(config.engineOpsRoot));
+});
+
+gulp.task('Transform-All-Engine-Env-Variables', function (cb) {
+    TransformSingleEngineEnvVariables(config.engineAuthoringRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatAuthoring');
+    TransformSingleEngineEnvVariables(config.engineShopsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatShops');
+    TransformSingleEngineEnvVariables(config.engineMinionsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatMinions');
+    TransformSingleEngineEnvVariables(config.engineOpsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'AdventureWorksOpsApi');
+    cb();
+});
+
+TransformSingleEngineEnvVariables = function (fileLocation, filename, jsonSelector, jsonValue) {
+    return gulp.src(fileLocation + "\\" + filename)
+        .pipe(jsonModify({ key: jsonSelector, value: jsonValue }))
+        .pipe(gulp.dest(fileLocation));
+};
+
+gulp.task('Publish-Commerce-Engine', function () {
+    return gulp.src(config.engineProjectPath + '/Sitecore.Commerce.Engine.csproj', { read: false })
+        .pipe(publish({
+            configuration: config.buildConfiguration,
+            output: "./bin/publish",
+            verbosity: config.buildVerbosity
+        }));
+});
+
+gulp.task('Delete-Existing-Engine-Files', function () {
+    return del(config.engineAuthoringRoot + "\\**\\*.*", { force: true },
+        del(config.engineOpsRoot + "\\**\\*.*", { force: true },
+            del(config.engineMinionsRoot + "\\**\\*.*", { force: true },
+                del(config.engineShopsRoot + "\\**\\*.*", { force: true }))));
+});
+
+
 
 gulp.task("01-Build-Commerce-Engine", function (callback) {
     var targets = ["Build"];
@@ -46,66 +107,65 @@ gulp.task("01-Build-Commerce-Engine", function (callback) {
         }));
 });
 
-gulp.task('02-Publish-Commerce-Engine-To-Instances', function(callback) {
-    return runSequence(
-       // "Stop-Local-IIS",
+gulp.task('02-Publish-Commerce-Engine-To-Instances', 
+    gulp.series(
+        "Stop-Local-IIS",
         "Publish-Commerce-Engine",
         "Delete-Existing-Engine-Files",
         "Copy-Published-Engine-To-All-Instances",
         "Transform-All-Engine-Env-Variables",
-        //"Start-Local-IIS",
-        callback
-    );
-});
+        "Start-Local-IIS", function(done) {
+            done();
+}));
 
-gulp.task('Copy-Published-Engine-To-All-Instances', function() {
-    return gulp.src(config.engineProjectPath + "/bin/publish/**/*")
-        .pipe(gulp.dest(config.engineAuthoringRoot))
-        .pipe(gulp.dest(config.engineShopsRoot))
-        .pipe(gulp.dest(config.engineMinionsRoot))
-        .pipe(gulp.dest(config.engineOpsRoot));
-});
+gulp.task('03-Publish-Website-Projects',
+    gulp.series(
+        "Publish-Foundation-Projects",
+        "Publish-Feature-Projects",
+        "Publish-Project-Projects", function (done) {
+            done();
+        }));
 
-gulp.task('Transform-All-Engine-Env-Variables', function(cb) {
-    TransformSingleEngineEnvVariables(config.engineAuthoringRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatAuthoring');
-    TransformSingleEngineEnvVariables(config.engineShopsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatShops');
-    TransformSingleEngineEnvVariables(config.engineMinionsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'HabitatMinions');
-    TransformSingleEngineEnvVariables(config.engineOpsRoot + "\\wwwroot", "config.json", 'AppSettings.EnvironmentName', 'AdventureWorksOpsApi');
-    cb();
-});
+gulp.task("default",
+    gulp.series(
+        "01-Build-Commerce-Engine",
+        "02-Publish-Commerce-Engine-To-Instances",
+        "03-Publish-Website-Projects", function (done) {
+            done();
+}));
 
-TransformSingleEngineEnvVariables = function(fileLocation, filename, jsonSelector, jsonValue) {
-    return gulp.src(fileLocation + "\\" + filename)
-        .pipe(jsonModify({ key: jsonSelector, value: jsonValue }))
-        .pipe(gulp.dest(fileLocation));
+
+var publishProjects = function (location) {
+    console.log("publish to " + config.sitecoreRoot + " folder");
+    return gulp.src([location + "/**/website/**/*.csproj"])
+        .pipe(flatmap(function (stream, file) {
+            return publishStream(stream);
+        }));
 };
 
-gulp.task('Publish-Commerce-Engine', function() {
-    return gulp.src(config.engineProjectPath + '/Sitecore.Commerce.Engine.csproj', { read: false })
-        .pipe(publish({
+var publishStream = function (stream) {
+    var targets = ["Build"];
+
+    return stream
+        .pipe(debug({ title: "Building project:" }))
+        .pipe(msbuild({
+            targets: targets,
             configuration: config.buildConfiguration,
-            output: "./bin/publish",
-            verbosity: config.buildVerbosity
+            logCommand: false,
+            verbosity: config.buildVerbosity,
+            stdout: true,
+            errorOnFail: true,
+            maxcpucount: config.buildMaxCpuCount,
+            nodeReuse: false,
+            toolsVersion: config.buildToolsVersion,
+            properties: {
+                Platform: config.publishPlatform,
+                DeployOnBuild: "true",
+                DeployDefaultTarget: "WebPublish",
+                WebPublishMethod: "FileSystem",
+                DeleteExistingFiles: "false",
+                publishUrl: config.sitecoreRoot,
+                _FindDependencies: "false"
+            }
         }));
-});
-
-gulp.task('Delete-Existing-Engine-Files', function() {
-    return del(config.engineAuthoringRoot + "\\**\\*.*", { force: true },
-            del(config.engineOpsRoot + "\\**\\*.*", { force: true },
-            del(config.engineMinionsRoot + "\\**\\*.*", { force: true },
-            del(config.engineShopsRoot + "\\**\\*.*", { force: true }))));
-});
-
-gulp.task('Stop-Local-IIS', function(callback) {
-    exec('Powershell.exe  iisreset /timeout:0 /stop', function(err, stdout) {
-        console.log(stdout);
-        callback(err);
-    });
-});
-
-gulp.task('Start-Local-IIS', function (callback) {
-    exec('Powershell.exe  iisreset /timeout:0 /start', function (err, stdout) {
-        console.log(stdout);
-        callback(err);
-    });
-});
+}
