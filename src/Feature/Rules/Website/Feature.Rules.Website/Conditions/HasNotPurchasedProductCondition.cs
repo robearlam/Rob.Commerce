60fc28e01b2bdbd37270eb8a1cbfeacd0c6fe351
Rob Earlam <robert.earlam@gmail.com>
2018-08-22 16:15:43 +1000

    Created Feature.Rules module

---
+++ src/Feature/Rules/Website/Feature.Rules.Website/Conditions/HasNotPurchasedProductCondition.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Sitecore.Rules;
+
+namespace Feature.Rules.Website.Conditions
+{
+    public class HasNotPurchasedProductCondition<T> : BaseOrderCondition<T> where T : RuleContext
+    {
+        public string PurchasedProductId { get; set; }
+        public int Days { get; set; }
+        protected override bool Execute(T ruleContext)
+        {
+            if (Sitecore.Context.IsLoggedIn)
+                return ValidateCase(PurchasedProductId, Days);
+            else
+                return true;
+        }
+
+        protected bool ValidateCase(string productId, int pastDaysAmount)
+        {
+            var orderOutcomes = GetSubmittedOrderOutcomes(pastDaysAmount);
+            if (orderOutcomes.Count() > 0)
+            {
+                if (orderOutcomes.Where(o => o.Order.CartLines.Where(c => (c.Product.ProductId.Contains("|") ? c.Product.ProductId.Split('|')[1] : c.Product.ProductId) == productId).FirstOrDefault() != null).FirstOrDefault() == null)
+                    return true;
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
