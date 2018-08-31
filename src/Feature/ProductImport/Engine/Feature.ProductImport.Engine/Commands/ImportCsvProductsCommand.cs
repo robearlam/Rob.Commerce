50e4872b4508b577556db16583417ba0b398f64b
Rob Earlam <robert.earlam@gmail.com>
2018-08-31 16:56:13 +1000

    Stubbed out Product Import controller & command

---
+++ src/Feature/ProductImport/Engine/Feature.ProductImport.Engine/Commands/ImportCsvProductsCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Core.Commands;
+
+namespace Feature.ProductImport.Engine.Commands
+{
+    public class ImportCsvProductsCommand : CommerceCommand
+    {
+        public async Task<CommerceCommand> Process(CommerceContext commerceContext, IFormFile importFile, string mode, bool publishEntities = true)
+        {
+            throw new NotImplementedException("Not implemented yet");
+        }
+    }
+}
---
+++ src/Feature/ProductImport/Engine/Feature.ProductImport.Engine/Controllers/CommandsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Web.Http.OData;
+using Feature.ProductImport.Engine.Commands;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Sitecore.Commerce.Core;
+
+namespace Feature.ProductImport.Engine.Controllers
+{
+    public class CommandsController : CommerceController
+    {
+        public CommandsController(IServiceProvider serviceProvider, CommerceEnvironment globalEnvironment) : base(serviceProvider, globalEnvironment)
+        {
+        }
+
+        [HttpPut]
+        [DisableRequestSizeLimit]
+        [Route("ImportCsvProducts()")]
+        public IActionResult ImportCsvProducts([FromBody] ODataActionParameters value)
+        {
+            if (!ModelState.IsValid || value == null)
+                return new BadRequestObjectResult(ModelState);
+
+            if (!value.ContainsKey("importFile"))
+                return new BadRequestObjectResult(value);
+
+            if(!(value["importFile"] is FormFile formFile))
+                return new BadRequestObjectResult("Import file not correct type");
+
+            var memoryStream = new MemoryStream();
+            formFile.CopyTo(memoryStream);
+
+            var file = new FormFile(memoryStream, 0L, formFile.Length, formFile.Name, formFile.FileName);
+            var updateMode = value["mode"].ToString();
+
+            var publishEntities = true;
+            if (value.ContainsKey("publish") && bool.TryParse(value["publish"].ToString(), out var result))
+                publishEntities = result;
+
+            return new ObjectResult(ExecuteLongRunningCommand(() => Command<ImportCsvProductsCommand>().Process(CurrentContext, file, updateMode,  publishEntities)));
+        }
+    }
+}
